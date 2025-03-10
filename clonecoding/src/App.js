d5598dd1d9dc6bae9025b90ed87880ea0f823169
ttttkii913 <dbgkdud821@daum.net>
2025-03-10 21:53:43 +0900

    ✨ Feat: 3장 실습 코드

--- clonecoding/src/App.js
+++ clonecoding/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Potato from "./Potato";
+
+function Food({ fav }) {
+  return <h1>I like { fav }</h1>
+}
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
+    </div>
+  );
 }
 
 export default App;
