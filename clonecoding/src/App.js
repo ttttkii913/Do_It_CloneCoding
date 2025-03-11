28b2a13394f13f82793b23413f5e92b67295f051
ttttkii913 <dbgkdud821@daum.net>
2025-03-11 19:53:14 +0900

    ✨ Feat: 5장 컴포넌트 실습 코드

--- clonecoding/src/App.js
+++ clonecoding/src/App.js
@@ -1,67 +1,49 @@
-import React from "react";
-import Potato from "./Potato";
-import PropTypes from 'prop-types'
+import React from 'react';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like { name }</h2>
-      <h4>{ rating }/5.0</h4>
-      <img src={picture} alt={name} />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
+
+  componentDidMount() {
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    console.log('I just updated');
+  }
+  
+  componentWillUnmount() {
+    console.log('Goodbye');
+  }
+
+  state = {
+    count: 0,
+  };
+
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  }
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return ( 
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'Kimchi',
-    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgyeopsal',
-    image:
-      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image:
-      'https://i.namu.wiki/i/EMil8lSAZGDYaLqY6oyHW1zpb2xu0EqfNPpGAAly5XPijG4O3pAbs1LjTyWaDCJ3cIhbpujgIrD9UM3ycLVVjoi99FkVs5JhaW7cDHSGGC5aRMLXkXpujmEk9kqQJte7uO4xSYeTP91kkVLCrlkbyw.jpg',
-    rating: 5,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
-    rating: 5,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
-    rating: 5,
-  },
-];
-
-function App() {
-  return (
-    <div>
-      { foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
-        ))} 
-      <Potato />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
 
 export default App;
