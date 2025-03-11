f21157a1fde37183ca70c8a8994e89ebb39c9d91
ttttkii913 <dbgkdud821@daum.net>
2025-03-11 18:27:09 +0900

    ✨ Feat: 4장

--- clonecoding/src/App.js
+++ clonecoding/src/App.js
@@ -1,21 +1,67 @@
 import React from "react";
 import Potato from "./Potato";
+import PropTypes from 'prop-types'
 
-function Food({ fav }) {
-  return <h1>I like { fav }</h1>
+function Food({ name, picture, rating }) {
+  return (
+    <div>
+      <h2>I like { name }</h2>
+      <h4>{ rating }/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  )
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'Kimchi',
+    image: 'http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg',
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+    image:
+      'https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg',
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: 'Bibimbap',
+    image:
+      'https://i.namu.wiki/i/EMil8lSAZGDYaLqY6oyHW1zpb2xu0EqfNPpGAAly5XPijG4O3pAbs1LjTyWaDCJ3cIhbpujgIrD9UM3ycLVVjoi99FkVs5JhaW7cDHSGGC5aRMLXkXpujmEk9kqQJte7uO4xSYeTP91kkVLCrlkbyw.jpg',
+    rating: 5,
+  },
+  {
+    id: 4,
+    name: 'Doncasu',
+    image: 'https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg',
+    rating: 5,
+  },
+  {
+    id: 5,
+    name: 'Kimbap',
+    image: 'http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg',
+    rating: 5,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      { foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+        ))} 
       <Potato />
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 export default App;
