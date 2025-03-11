f314b070552363210cda3e1f098f8d94c9715bf4
ttttkii913 <dbgkdud821@daum.net>
2025-03-11 20:39:55 +0900

    ✨ Feat: 6장 실습 코드

---
+++ clonecoding/src/Movie.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import './Movie.css';
+
+function Movie({ title, year, summary, poster }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+            
+            <div className="movie_data">
+                <h3 className="movie_title">{ title }</h3>
+                <h5 className="movie_year">{ year }</h5>
+                <p className="movie_summary">{ summary }</p>
+            </div>
+        </div>
+    );
+}
+
+Movie.propTypes = { 
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+};
+
+export default Movie;
