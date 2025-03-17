51b38ec998965ab89a5adc3335a9732f7de7b658
ttttkii913 <dbgkdud821@daum.net>
2025-03-17 18:57:59 +0900

    ✨ Feat: AOS 스크롤 적용

--- clonecoding/src/routes/Home.js
+++ clonecoding/src/routes/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Movie from '../components/Movie';
 import './Home.css';
 
@@ -12,41 +14,54 @@ class Home extends React.Component {
   getMovies = async () => {
     const {
       data: {
-        data: {movies},
+        data: { movies },
       },
     } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({ movies, isLoading: false });
-  }
+    
+    this.setState({ movies, isLoading: false }, () => {
+      AOS.init({ 
+        duration: 1000,  // 애니메이션 지속 시간
+        once: false,      // 스크롤 시 반복 실행 가능
+        mirror: true,
+        offset: 100,      // 화면에 나타나기 100px 전부터 실행
+      });
+    });
+  };
 
   componentDidMount() {
-    this.getMovies();  
+    this.getMovies();
   }
 
   render() {
     const { isLoading, movies } = this.state;
     return (
       <section className="container">
-      {isLoading ? (
-        <div className="loader">
-          <span className="loader_text">Loading...</span>
-        </div>
-      ) : (
-        <div className="movies">
-        { movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              year={movie.year}
-              title={movie.title}
-              summary={movie.summary}
-              poster={movie.medium_cover_image}
-              genres={movie.genres}
-            />
-        ))}
-        </div>
-      )}
-    </section>
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie, index) => (
+              <div key={movie.id} 
+                   className="movie-card"
+                   data-aos="fade-up" 
+                   data-aos-delay={index * 100}> {/* 순차적으로 등장 */}
+                <Movie
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
+
 export default Home;
